3e9ea35a679a897606834e141958cdf09df24c49
hanna rafael <hannanrafael@gmail.com>
2019-11-30 18:00:50 -0500

    update fixed

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Hanna Rafael',
+    profilePic: 'https://avatars2.githubusercontent.com/u/49074642?s=460&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
